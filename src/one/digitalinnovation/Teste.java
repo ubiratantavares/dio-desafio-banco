e464f22fa865b73dbe0cd40821bda845f24e4312
= <=>
2022-01-23 14:29:23 -0300

    inserindo a classe de teste

---
+++ src/one/digitalinnovation/Teste.java
@@ -0,0 +1,19 @@
+package one.digitalinnovation;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Teste {
+    public static void main(String[] args) throws IOException {
+
+        Scanner leitor = new Scanner(System.in);
+
+        int senha = leitor.nextInt();
+
+        while (senha != 2002) {
+            System.out.println("Senha Invalida");
+            senha = leitor.nextInt();
+        }
+        System.out.println("Acesso Permitido");
+    }
+}
